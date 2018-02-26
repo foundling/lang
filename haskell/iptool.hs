9e61387b4f34aba910e3a957d5d5342e45f3c56c
Alex Ramsdell <alexramsdell@gmail.com>
2018-02-25 18:02:26 -0800

    rename to iptool. add binToDec and use in hostnumber fn.

--- haskell/iptool.hs
+++ haskell/iptool.hs
@@ -8,10 +8,12 @@ notDot = (/=) '.'
 charsToDigit :: [Char] -> [Int]
 charsToDigit = map digitToInt 
 
+productOfPair :: (Int, Int) -> Int  
+productOfPair (a,b) = a * b
+
 digitsToDecimal :: [Int] -> Int
 digitsToDecimal digits = sum $ map productOfPair $ zip reversedDigits powersOfTen
     where 
-        productOfPair = (\ x -> (fst x) * (snd x)) 
         reversedDigits = reverse digits
         powersOfTen = iterate (*10) 1
 
@@ -29,10 +31,24 @@ decToBin x = (decToBin dividend) ++ [remainder]
     where
         (dividend, remainder) = divMod x 2 
 
+binToDec :: [Int] -> Int
+binToDec bits = sum $ map productOfPair (zip (reverse bits) $ iterate (*2) 1)
+
 makeByte :: [Int] -> [Int] 
 makeByte bits = (replicate n 0) ++ bits 
     where
         n = 8 - (length bits)
 
-toBytes ip = map makeByte $ map decToBin (octets ip)
+toBits :: String -> [Int]
+toBits ip = concat $ map makeByte $ map decToBin (octets ip)
 
+bitmask :: Int -> Int -> [Int]
+bitmask n maskValue = take 32 $ (replicate n maskValue) ++ (repeat complement)  
+    where
+        complement = (maskValue + 1) `mod` 2
+
+gateway :: String -> Int -> [Int]
+gateway ip networkSize = map productOfPair $ zip (toBits ip) (bitmask networkSize 1)
+
+hostNumber :: String -> Int  -> Int
+hostNumber ip networkSize = binToDec $ map productOfPair $ zip (toBits ip) (bitmask networkSize 0)  
